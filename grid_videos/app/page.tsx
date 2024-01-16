1532a3dc22b8fbcb9d3d4ba16783fbf98fb74760
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2024-01-15 22:08:51 -0300

    ailwindcss study

--- grid_videos/app/page.tsx
+++ grid_videos/app/page.tsx
@@ -1,8 +1,10 @@
+import Collections from "@/components/Collections";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <span>Grid Video</span>
+        <Collections />
       </div>
     </main>
   )
---
+++ grid_videos/components/Collections.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+import PopUp from "./PopUp";
+
+const movies = [
+    {
+      name: 'Desk and Office',
+      description: 'Work from home accessories',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
+      imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
+      href: '#',
+    },
+    {
+      name: 'Self-Improvement',
+      description: 'Journals and note-taking',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
+      imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
+      href: '#',
+    },
+    {
+      name: 'Travel',
+      description: 'Daily commute essentials',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
+      imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+      href: '#',
+    },
+    {
+        name: 'Travel',
+        description: 'Daily commute essentials',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
+        imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+        href: '#',
+    },
+    {
+        name: 'Travel',
+        description: 'Daily commute essentials',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
+        imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+        href: '#',
+    },
+    {
+        name: 'Travel',
+        description: 'Daily commute essentials',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
+        imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+        href: '#',
+    },
+    {
+        name: 'Travel',
+        description: 'Daily commute essentials',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
+        imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+        href: '#',
+    },
+    {
+        name: 'Travel',
+        description: 'Daily commute essentials',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
+        imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+        href: '#',
+    },
+    {
+        name: 'Travel',
+        description: 'Daily commute essentials',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
+        imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+        href: '#',
+      },
+]
+  
+const buttonAction = () => {
+    console.log('Button clicked')
+}
+  
+const Collections = () => {
+    return (
+      <div className="bg-gray-100">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
+            <h2 className="text-2xl font-bold text-gray-900">Grid Videos</h2>
+  
+            <div className="mt-6 space-y-12 lg:grid lg:grid-cols-4 lg:gap-x-6 lg:space-y-0">
+              {movies.map((movie, index) => (
+                <div key={movie.name} className="group relative">
+                  <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                          
+                        <img src={movie.imageSrc} alt={movie.imageAlt}
+                        className="h-full w-full object-cover object-center" />
+                  </div>
+                  <h3 className="mt-6 text-sm text-gray-500">
+                      <span className="absolute inset-0" />
+                      {movie.name}
+                  </h3>
+                  <p className="text-base font-semibold text-gray-900">{movie.description}</p>
+                </div>
+              ))}
+              <PopUp />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+  
+export default Collections;
