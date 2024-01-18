db2c29d5d76cd9333d6b835140b2606111458d76
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2024-01-17 21:47:43 -0300

    init components

--- grid_videos/app/page.tsx
+++ grid_videos/app/page.tsx
@@ -1,10 +1,41 @@
-import Collections from "@/components/Collections";
+import CardMovie from "@/components/CardMovie";
+import PopUp from "@/components/PopUp";
+import { Dialog } from "@mui/material";
+
+const movies = [
+  {
+    title: "A volta dos que não foram",
+    thumb: "https://placehold.it/300x200",
+    video: "https://www.youtube.com/watch?v=7Qp5vcuMIlk",
+  },
+  {
+    title: "A volta dos que não foram",
+    thumb: "https://placehold.it/300x200",
+    video: "https://www.youtube.com/watch?v=7Qp5vcuMIlk",
+  },
+  {
+    title: "A volta dos que não foram",
+    thumb: "https://placehold.it/300x200",
+    video: "https://www.youtube.com/watch?v=7Qp5vcuMIlk",
+  },
+  {
+    title: "A volta dos que não foram",
+    thumb: "https://placehold.it/300x200",
+    video: "https://www.youtube.com/watch?v=7Qp5vcuMIlk",
+  },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <Collections />
+    <main className=" p-24">
+        <h1>Clone Youtube</h1>
+      <div className="grid grid-cols-5 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mx-auto">
+        {movies.map((movie) => ( 
+          <>
+          <CardMovie />
+          <PopUp />
+          </>
+        ))}
       </div>
     </main>
   )
